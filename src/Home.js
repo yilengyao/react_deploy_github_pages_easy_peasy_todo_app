f19b3d670d683fdb3430046bc811e70d6d23032c
YilengYao <yilengyao@gmail.com>
2023-10-05 15:03:47 -0700

    rename url

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import Feed from "./Feed";
+import { useStoreState } from "easy-peasy";
+
+const Home = ({ isLoading, fetchError }) => {
+  const searchResults = useStoreState((state) => state.searchResults);
+
+  return (
+    <main className="Home">
+      {isLoading && <p className="loading">Loading posts...</p>}
+      {!isLoading && fetchError && (
+        <p className="statusMsg" style={{ color: "red" }}>
+          {fetchError}
+        </p>
+      )}
+      {!isLoading &&
+        !fetchError &&
+        (searchResults.length ? (
+          <Feed posts={searchResults} />
+        ) : (
+          <p className="statusMsg">No posts to display.</p>
+        ))}
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/PostPage.js
@@ -0,0 +1,50 @@
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useStoreState, useStoreActions } from "easy-peasy";
+
+const PostPage = () => {
+  const { id } = useParams();
+  const deletePost = useStoreActions((actions) => actions.deletePost);
+  const getPostById = useStoreState((state) => state.getPostById);
+
+  const navigate = useNavigate();
+
+  const post = getPostById(id);
+  const handleDelete = async (id) => {
+    deletePost(id);
+    navigate("/");
+  };
+
+  return (
+    <main className="PostPage">
+      <article className="post">
+        {post && (
+          <>
+            <h2>{post.title}</h2>
+            <p className="postDate">{post.date}</p>
+            <p className="postBody">{post.body}</p>
+            <Link to={`/edit/${post.id}`}>
+              <button className="editButton">Edit Post</button>
+            </Link>
+            <button
+              className="Delete Button"
+              onClick={() => handleDelete(post.id)}
+            >
+              Delete Post
+            </button>
+          </>
+        )}
+        {!post && (
+          <>
+            <h2>Post Not Found</h2>
+            <p>Well, that's disappointing.</p>
+            <p>
+              <Link to="/">Visit Our Homepage</Link>
+            </p>
+          </>
+        )}
+      </article>
+    </main>
+  );
+};
+
+export default PostPage;
